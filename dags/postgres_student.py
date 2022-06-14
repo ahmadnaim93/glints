3ebbdec23b8b5df6f2ed6846b2d6f906559b21d2
Naim Rosdi <ahmad.naim.93@gmail.com>
2022-06-14 18:05:22 +0800

    Initial commit

---
+++ dags/postgres_student.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.postgres_operator import PostgresOperator
+from airflow.hooks.postgres_hook import PostgresHook
+dag_params = {
+    'dag_id': 'postgres_student_migration'
+}
+with DAG(**dag_params) as dag:
+    src = PostgresHook(postgres_conn_id='Source')
+    dest = PostgresHook(postgres_conn_id='Destination')
+    src_conn = src.get_conn()
+    cursor = src_conn.cursor()
+    dest_conn = dest.get_conn()
+    dest_cursor = dest_conn.cursor()
+    dest_cursor.execute("SELECT CAST(MAX(S_ID) AS INTEGER) FROM STUDENT;")
+    student_id = dest_cursor.fetchone()[0]
+    if student_id is None:
+        student_id = 0
+    cursor.execute("SELECT s_id, s_name, s_birth, s_sex, CURRENT_TIMESTAMP as s_insert_dt  FROM STUDENT WHERE CAST(S_ID AS INT) > %s", [student_id])
+    dest.insert_rows(table="STUDENT", rows=cursor)
---
+++ readme.md
@@ -0,0 +1,11 @@
+Setup
+1. Run in terminal
+    $ docker-compose up
+
+2. Login to PostgreSQL database using pgAdmin. Check credential file
+
+3. Login to destination database. Check existing student records.
+
+4. Logint to source database. Insert new student records. Use student.sql in sql folder
+
+3. Check again destination database. New records from source database is copied in destination database.
