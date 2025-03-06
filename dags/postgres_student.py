ee808e48b33a566728bb1bd44376b11588c266e0
Naim <ahmad.naim.93@gmail.com>
2025-03-06 16:59:06 +0800

    Update

--- dags/postgres_student.py
+++ dags/postgres_student.py
@@ -1,19 +1,46 @@
 from airflow import DAG
-from airflow.operators.postgres_operator import PostgresOperator
-from airflow.hooks.postgres_hook import PostgresHook
-dag_params = {
-    'dag_id': 'postgres_student_migration'
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
 }
-with DAG(**dag_params) as dag:
-    src = PostgresHook(postgres_conn_id='source')
-    dest = PostgresHook(postgres_conn_id='destination')
-    src_conn = src.get_conn()
-    cursor = src_conn.cursor()
-    dest_conn = dest.get_conn()
-    dest_cursor = dest_conn.cursor()
-    dest_cursor.execute("SELECT CAST(MAX(S_ID) AS INTEGER) FROM STUDENT;")
-    student_id = dest_cursor.fetchone()[0]
-    if student_id is None:
-        student_id = 0
-    cursor.execute("SELECT s_id, s_name, s_birth, s_sex, CURRENT_TIMESTAMP as s_insert_dt  FROM STUDENT WHERE CAST(S_ID AS INT) > %s", [student_id])
-    dest.insert_rows(table="STUDENT", rows=cursor)
+
+dag = DAG(
+    'copy_table_postgresql',
+    default_args=default_args,
+    description='A DAG to copy a table from one PostgreSQL instance to another',
+    schedule_interval=None,
+    start_date=days_ago(1),
+    tags=['example'],
+)
+
+start = DummyOperator(
+    task_id='start',
+    dag=dag,
+)
+
+# Extract data from source PostgreSQL
+extract_task = PostgresOperator(
+    task_id='extract_data',
+    postgres_conn_id='source',
+    sql='COPY source_table TO STDOUT WITH CSV HEADER',
+    filename='/tmp/source_table.csv',
+    dag=dag,
+)
+
+# Load data into destination PostgreSQL
+load_task = PostgresOperator(
+    task_id='load_data',
+    postgres_conn_id='destination',
+    sql='COPY destination_table FROM STDIN WITH CSV HEADER',
+    filename='/tmp/source_table.csv',
+    dag=dag,
+)
+
+start >> extract_task >> load_task
